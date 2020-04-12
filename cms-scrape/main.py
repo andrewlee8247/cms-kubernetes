18fde0f81be9895d1d14f0ecbf1aec254972c65a
andrewlee8247 <andrewlee2015@u.northwestern.edu>
2020-04-12 09:42:50 -0700

    add scraping files

---
+++ cms-scrape/main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+import scraper
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def scrape():
+    scraper.parseFiles()
+    return jsonify({'status': 'scraping complete'})
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=80)
