24cf94fb1d43e943d0aa96b26df154cb8b328bba
andrewlee8247 <andrewlee2015@u.northwestern.edu>
2020-05-12 07:56:25 -0700

    test changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,10 +2,13 @@ import sys
 sys.path.append('./')
 from app.api.api import app
 
+print('=====================API test session starts======================')
 try:
     with app.test_client() as c:
         response = c.get('/api/')
         assert response.status_code == 200
     print('Response Successful')
+    print('========================API test passed===========================')
 except Exception as e:
     print('Response Error {}'.format(str(e)))
+    print('========================API test failed===========================')
--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -2,10 +2,13 @@ import sys
 sys.path.append('./')
 from app.frontend.app import server
 
+print('===================front-end test session starts====================')
 try:
     with server.test_client() as c:
         response = c.get('/')
         assert response.status_code == 200
     print('Response Successful')
+    print('=======================front-end test passed========================')
 except Exception as e:
     print('Response Error {}'.format(str(e)))
+    print('=======================front-end test failed========================')
