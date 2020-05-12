bde57536159b51618efb094bfcefa3aba6e3942e
andrewlee8247 <andrewlee2015@u.northwestern.edu>
2020-05-12 08:12:19 -0700

    test changes

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,8 @@ try:
     with app.test_client() as c:
         response = c.get('/api/')
         assert response.status_code == 200
-    print('Response Successful')
+    print('\n Response Successful')
     print('\n =============================== API test passed ================================')
 except Exception as e:
-    print('Response Error {}'.format(str(e)))
+    print('\n Response Error {}'.format(str(e)))
     print('\n =============================== API test failed ================================')
